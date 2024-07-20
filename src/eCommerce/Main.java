52f796bf2a7b806352be1792924ba30f097116f0
senablgn <senabilgin17@gmail.com>
2024-07-20 20:03:06 +0300

    Engin Demiroğ java lessons - Day 5 - Homework1

--- src/eCommerce/Main.java
+++ src/eCommerce/Main.java
@@ -10,11 +10,11 @@ import eCommerce.entities.concretes.User;
 public class Main {
 
 	public static void main(String[] args) {
-		 User user=new User(2, "sena", "bilgin", "sena@gmail.com", "123456");
+		 User user=new User(2, "Sena", "Bilgin", "test@gmail.com", "123456");
 		 UserService userService=new UserManager(new HibernateUserDao(),new EmailManager(),new GoogleRegisterManagerAdapter());
 		 userService.register(user);
 		 userService.registerWithGoogle(user);
-		 userService.logIn(user, "sena@gmail.com", "123456");
+		 userService.logIn(user, "test@gmail.com", "123456");
 
 	}
 
